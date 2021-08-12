7fafb61c95621e19c4eb049dc921d305c70fb355
Flavio Danesse <fdanesse@gmail.com>
2021-08-12 16:58:59 -0300

    Conexiones de Sockets multiples

--- src/app/aulas-remotas/aulas-remotas.module.ts
+++ src/app/aulas-remotas/aulas-remotas.module.ts
@@ -6,19 +6,8 @@ import { AulasRemotasComponent } from './Views/aulas-remotas/aulas-remotas.compo
 import { AulasRemotasRoutingModule } from './aulas-remotas-routing.module';
 
 import { AulasRemotasSocket } from './services/AulasRemotasSocket.service';
-import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { SocketIoModule } from 'ngx-socket-io';
 import { AulaComponent } from './Views/aula/aula.component';
-
-const config: SocketIoConfig = {
-    url: 'http://localhost:8080', //'https://lit-fortress-19290.herokuapp.com',
-    options: {
-        withCredentials: false,
-        rememberUpgrade:true,
-        transports: ['websocket'],
-        secure:true, 
-        rejectUnauthorized: false
-    }}
-//options corrige => Access to XMLHttpRequest at 'http://localhost:8080/socket.io/?EIO=3&transport=polling&t=Nhk9XQr' from origin 'http://localhost:4200' has been blocked by CORS policy: The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'. The credentials mode of requests initiated by the XMLHttpRequest is controlled by the withCredentials attribute.
 
 
 @NgModule({
@@ -29,7 +18,7 @@ const config: SocketIoConfig = {
         CommonModule,
         ReactiveFormsModule,
         AulasRemotasRoutingModule,
-        SocketIoModule.forRoot(config),
+        SocketIoModule,
     ],
     providers: [AulasRemotasSocket], //AuthGuard
 })
--- src/app/aulas-remotas/services/AulasRemotasSocket.service.ts
+++ src/app/aulas-remotas/services/AulasRemotasSocket.service.ts
@@ -2,13 +2,24 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Subscription } from "rxjs";
 
 // https://www.npmjs.com/package/ngx-socket-io
-import { Socket } from 'ngx-socket-io';
+import { Socket, SocketIoConfig } from 'ngx-socket-io';
 
 import { UsersService } from '../../auth/Services/users.service';
 
+const config: SocketIoConfig = {
+    url: 'http://localhost:8080', //'https://lit-fortress-19290.herokuapp.com',
+    options: {
+        withCredentials: false,
+        rememberUpgrade:true,
+        transports: ['websocket'],
+        secure:true, 
+        rejectUnauthorized: false
+    }}
+//options corrige => Access to XMLHttpRequest at 'http://localhost:8080/socket.io/?EIO=3&transport=polling&t=Nhk9XQr' from origin 'http://localhost:4200' has been blocked by CORS policy: The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'. The credentials mode of requests initiated by the XMLHttpRequest is controlled by the withCredentials attribute.
+
 
 @Injectable({ providedIn: 'root' })
-export class AulasRemotasSocket {
+export class AulasRemotasSocket extends Socket{
 
     token = '';
     userLoggued = null;
@@ -25,7 +36,9 @@ export class AulasRemotasSocket {
     private obsNewRespuesta = new BehaviorSubject(null);
     public newRespuesta = this.obsNewRespuesta.asObservable();
 
-    constructor(private socket: Socket, private userService: UsersService) {
+    constructor(private userService: UsersService) {
+        super(config);
+
         // FIXME: Resolver el uso del token y usuario logueado
         this.tokenSubscription = this.userService.obsToken.subscribe(
             res => {
@@ -47,52 +60,52 @@ export class AulasRemotasSocket {
     }
 
     processEvents(){
-        this.socket.on('connect', () => {
+        this.on('connect', () => {
             // FIXME: que no se envie nada antes de que esto se ejecute.
             console.log('Socket conectado');
         });
 
-        this.socket.on("disconnect", (reason) => {
+        this.on("disconnect", (reason) => {
             // FIXME: Resolver desconexión de socket local
             console.log('Socket desconectado:', reason);
         });
 
-        this.socket.on("connect_error", (error) => {
+        this.on("connect_error", (error) => {
             // FIXME: Resolver este error de conexión
             console.log('Socket error de conexión:', error);
         });
         
 
 
-        this.socket.on("oferta", (data) => {
+        this.on("oferta", (data) => {
             this.obsNewOffer.next(data);
         });
 
-        this.socket.on("respuesta", (data) => {
+        this.on("respuesta", (data) => {
             this.obsNewRespuesta.next(data);
         });
 
-        this.socket.on("candidato", (data) => {
+        this.on("candidato", (data) => {
             this.obsNewCandidate.next(data);
         });
 
         // FIXME: Completar tarea
-        this.socket.on("desconectado", (socketId) => {
+        this.on("desconectado", (socketId) => {
             console.log('Usuario desconectado', socketId);
         });
 
     }
 
     public enviarRespuesta(localDescription){
-        this.socket.emit('respuesta', localDescription);
+        this.emit('respuesta', localDescription);
     }
 
     public enviarOferta(aula, localDescription){
-        this.socket.emit('oferta', {'aula': aula, 'sdp': localDescription});
+        this.emit('oferta', {'aula': aula, 'sdp': localDescription});
     }
 
     public enviarCandidato(candidato){
-        this.socket.emit('candidato', candidato);
+        this.emit('candidato', candidato);
     }
 
     ngOnDestroy(){
